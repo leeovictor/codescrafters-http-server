566c7ffaa08bc26f4e5cb47d7ba53a99c28c0665
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-10 14:03:06 -0300

    stage 4

--- app/main.js
+++ app/main.js
@@ -1,17 +1,26 @@
 const net = require("net");
 const PORT = 4221;
 
+const requestParser = (buffer) => {
+  const lines = buffer.toString().split('\r\n');
+  const [method, path, protocol] = lines[0].split(' ');
+  return { method, path, protocol };
+}
+
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    const dataStr = data.toString();
-    const lines = dataStr.split("\r\n");
-    const url = lines[0].split(' ')[1];
-    if (url !== '/') {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+  socket.on('data', (buffer) => {
+    const request = requestParser(buffer);
+    console.log(request)
+
+    if (request.path === '/') {
+      socket.end("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (/^\/echo\/[^/]+$/.test(request.path)) {
+      const echoStr = request.path.split('/')[2];
+      const length = Buffer.byteLength(echoStr)
+      socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${echoStr}`);
     } else {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.end("HTTP/1.1 404 Not Found\r\n\r\n")
     }
-    socket.end();
   });
 });
 
