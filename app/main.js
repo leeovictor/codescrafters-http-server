d8f0bfab30dc9511d1699b55098249c5545bb8ef
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-10 00:54:47 -0300

    write socket

--- app/main.js
+++ app/main.js
@@ -1,14 +1,15 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  console.log('client connected');
+
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.on("close", () => {
     socket.end();
     server.close();
   });
 });
 
+console.log('Listening on port 4221...')
 server.listen(4221, "localhost");
