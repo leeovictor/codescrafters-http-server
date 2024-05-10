9d8f952d6a5f0ff1617a00c448ab16b8342491cf
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-10 01:19:28 -0300

    404 response

--- app/main.js
+++ app/main.js
@@ -2,13 +2,17 @@ const net = require("net");
 const PORT = 4221;
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on('data', (data) => {
+    const dataStr = data.toString();
+    const lines = dataStr.split("\r\n");
+    const url = lines[0].split(' ')[1];
+    if (url !== '/') {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    } else {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
     socket.end();
-    server.close();
   });
-
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.end();
 });
 
 server.listen({ port: PORT, hostname: 'localhost' }, () => {
