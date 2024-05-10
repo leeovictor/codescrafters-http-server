dbe24261a3b5a27d065d172c436a940a7e87b6dd
Leonardo Victor <leonardovictormarques@gmail.com>
2024-05-10 15:02:43 -0300

    stage 5

--- app/main.js
+++ app/main.js
@@ -1,16 +1,28 @@
 const net = require("net");
 const PORT = 4221;
 
-const requestParser = (buffer) => {
-  const lines = buffer.toString().split('\r\n');
-  const [method, path, protocol] = lines[0].split(' ');
-  return { method, path, protocol };
+const requestParser = (data) => {
+  const lines = data.toString().split('\r\n');
+  const [startLine, ...headersLines] = lines;
+  const [method, path, protocol] = startLine.split(' ');
+  
+  const headers = headersLines
+    .filter(value => Boolean(value))
+    .reduce((acc, cur) => {
+      const colonIndex = cur.indexOf(':');
+      const headerName = cur.substring(0, colonIndex);
+      const headerValue = cur.substring(colonIndex).replace(': ', '').trim(); // FIX
+      acc[headerName] = headerValue;
+      return acc;
+    }, {});
+  
+  return { method, path, protocol, headers };
 }
 
 const server = net.createServer((socket) => {
-  socket.on('data', (buffer) => {
-    const request = requestParser(buffer);
-    console.log(request)
+  socket.on('data', (data) => {
+    const request = requestParser(data);
+    console.log(request);
 
     if (request.path === '/') {
       socket.end("HTTP/1.1 200 OK\r\n\r\n");
@@ -18,6 +30,10 @@ const server = net.createServer((socket) => {
       const echoStr = request.path.split('/')[2];
       const length = Buffer.byteLength(echoStr)
       socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${echoStr}`);
+    } else if (request.path === '/user-agent') {
+      const userAgent = request.headers['User-Agent'];
+      const length = Buffer.byteLength(userAgent)
+      socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${userAgent}`);
     } else {
       socket.end("HTTP/1.1 404 Not Found\r\n\r\n")
     }
